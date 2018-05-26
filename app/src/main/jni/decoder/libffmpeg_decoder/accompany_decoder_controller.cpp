cc4e088f42cf487fe647e01950c58fd4bb158c12
ChenQingZhen <1038990951@qq.com>
2018-05-26 19:06:57 +0800

    1、将项目转成AS结构 2、 修改版本，toolchain从4.8改成4.9 NDK_TOOLCHAIN_VERSION = 4.9 最低兼容编译平台从android-9改成android-14 APP_PLATFORM := android-14 3、FIX BUG Mp3Decoder 的init返回始终不成功

---
+++ app/src/main/jni/decoder/libffmpeg_decoder/accompany_decoder_controller.cpp
@@ -0,0 +1,49 @@
+#include "accompany_decoder_controller.h"
+
+#define LOG_TAG "AccompanyDecoderController"
+
+AccompanyDecoderController::AccompanyDecoderController() {
+	accompanyDecoder = NULL;
+	pcmFile = NULL;
+}
+
+AccompanyDecoderController::~AccompanyDecoderController() {
+}
+
+int AccompanyDecoderController::Init(const char* accompanyPath, const char* pcmFilePath) {
+	//初始化两个decoder
+	AccompanyDecoder* tempDecoder = new AccompanyDecoder();
+	int accompanyMetaData[2];
+	tempDecoder->getMusicMeta(accompanyPath, accompanyMetaData);
+	delete tempDecoder;
+	//初始化伴奏的采样率
+	accompanySampleRate = accompanyMetaData[0];
+	int accompanyByteCountPerSec = accompanySampleRate * CHANNEL_PER_FRAME * BITS_PER_CHANNEL / BITS_PER_BYTE;
+	accompanyPacketBufferSize = (int) ((accompanyByteCountPerSec / 2) * 0.2);
+	accompanyDecoder = new AccompanyDecoder();
+	int ret= accompanyDecoder->init(accompanyPath, accompanyPacketBufferSize);
+	pcmFile = fopen(pcmFilePath, "wb+");
+	return pcmFile!=NULL&&ret==1?1:-1;
+}
+
+void AccompanyDecoderController::Decode() {
+	while(true) {
+		AudioPacket* accompanyPacket = accompanyDecoder->decodePacket();
+		if(-1 == accompanyPacket->size) {
+			break;
+		}
+		fwrite(accompanyPacket->buffer, sizeof(short), accompanyPacket->size, pcmFile);
+	}
+}
+
+void AccompanyDecoderController::Destroy() {
+	if (NULL != accompanyDecoder) {
+		accompanyDecoder->destroy();
+		delete accompanyDecoder;
+		accompanyDecoder = NULL;
+	}
+	if(NULL != pcmFile) {
+		fclose(pcmFile);
+		pcmFile = NULL;
+	}
+}
